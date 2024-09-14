211bdc71d03adc419ae33b36d5baeb662e14880e
Duckman <David.Ca3545@gmail.com>
2024-09-13 21:39:40 -0700

    Added a boolean function directory for practice

---
+++ boolean_function/boolean_function.v
@@ -0,0 +1,9 @@
+module boolean_function(input a, input b, input c, output z);
+
+  wire p;
+  wire q;
+
+  assign p = a & ~b;
+  assign q = ~a & c;
+  assign z = p | q;
+endmodule
---
+++ boolean_function/boolean_function_tb.v
@@ -0,0 +1,23 @@
+module boolean_function_tb();
+  reg a_tb;
+  reg b_tb;
+  reg c_tb;
+
+  wire z_tb;
+
+  boolean_function uut(.a(a_tb), .b(b_tb), .c(c_tb), .z(z_tb));
+
+  initial begin
+    a_tb = 1;
+    b_tb = 1;
+    c_tb = 1;
+    #100;
+  end
+
+  initial begin
+    #100 
+    $display("Result: z=%d", z_tb);
+    $finish;
+  end
+
+endmodule
