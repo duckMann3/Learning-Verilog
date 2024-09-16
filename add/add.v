676016466365471f4e3b51b8f965bbae2778dd0d
Duckman <David.Ca3545@gmail.com>
2024-09-16 09:48:34 -0700

    Changes to add

--- add/add.v
+++ add/add.v
@@ -1,8 +1,7 @@
 module add(
-  input [2:0] in0,
-  input [2:0] in1,
   output [2:0] out,
-  output carry
+  input [2:0] in0, in1,
+  output carry, overflow_flag
 );
 
   wire [5:0] temp_out;
@@ -10,5 +9,44 @@ module add(
   assign temp_out = in0 + in1;
   assign out = temp_out[2:0];
   assign carry = temp_out[3];
+
+  assign overflow_flag = carry & (in0[0] ^ in1[0]);
+
+  /* How 'overflow_flag' should work:
+  * NOTE: Carry
+  * 1 + 1 = 0, carry 1
+  * 1 + 0 or 0 + 1 = 1, carry 0
+  * 1 + 1 + 1 = 1, carry 1
+  * 1 + 1 + 1 + 1 = 1
+  *
+  * using 3-bit binary, unsinged numbers...
+  * 111 + 001 = ?
+  *
+  * Carry:    1 1 1 0
+  * Input1:     1 1 1 (7)
+  * Input2: +   0 0 1 (1)
+  * ------------------
+  * Result:   1|0 0 0
+  *
+  * using 4-bit binary, signed numbers...
+  * 111 + 001 = ?
+  *
+  * Carry:    0 1 1 1 0
+  * Input1:     0 1 1 1 (7)
+  * Input2: +   0 0 0 1 (1)
+  * --------------------
+  * Result:   0|1 0 0 0
+  *
+  * using 3-bit binary, singed numbers...
+  * 111 + 001 = ?
+  *
+  * Carry:    0 1 1
+  * Input1:     1 1 1 (-1)
+  * Input2: +   0 0 1 (1) -> Convert: 101; Invert: 010 + 001 = 011 aka -3
+  * ------------------
+    * Result: 1|0 0 0 -> Convert: 100; Invert: 011 + 001 = 100 aka -4 
+    *                 -> Convert: 1100; Invert: 0011 + 0001 = 0100 aka -4
+  * */
+
 
 endmodule
--- add/add_tb.v
+++ add/add_tb.v
@@ -1,21 +1,24 @@
 module add_beh_sim();
-  reg[2:0] in0_tb;
-  reg[2:0] in1_tb;
+  reg signed[2:0] in0_tb, in1_tb;
+  wire signed [2:0] out_tb;
+  wire carry_tb, overflow_flag_tb;
 
-  wire [2:0] out_tb;
-  wire carry_tb;
-
-  add uut(.in0(in0_tb), .in1(in1_tb), .out(out_tb), .carry(carry_tb));
+  add uut(.in0(in0_tb), .in1(in1_tb), .out(out_tb), .carry(carry_tb), .overflow_flag(overflow_flag_tb));
 
   initial begin
-    in0_tb = 3'b000;
+    in0_tb = 3'b111;
     in1_tb = 3'b001;
     #100;
   end
 
   initial begin
-    #100 
-    $display("Result: %d", out_tb);
+    #100;
+    $display("Adding: %d + %d", in0_tb, in1_tb);
+    $display("Result: %d, Carry: %d", out_tb, carry_tb);
+    #100;
+    if(overflow_flag_tb == 0) begin
+      $display("Overflow! %d", overflow_flag_tb);
+    end
     $finish;
   end
 endmodule
